9548de81ffc57adaa0ffe8c63f4752ad27707dd1
carolsfim <cacausfim@gmail.com>
2025-07-24 15:56:57 -0300

    Add .next/static/chunks/src_app_ideas_page_tsx_a993527f._.js 1753383417720

---
+++ .next/static/chunks/src_app_ideas_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_b3ec3dfb._.js",
+  "static/chunks/node_modules_53034175._.js"
+],
+    source: "dynamic"
+});
